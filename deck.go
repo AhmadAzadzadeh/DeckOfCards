1b56cb50e0a0cc9093d5ec52f4e772d5e9f2b4be
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-07-08 11:56:34 +0430

    create saveToFile receiver function for saving cards in to the file

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -35,4 +36,8 @@ func deal(d deck, handSize int) (deck, deck) {
 // receiver function
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
