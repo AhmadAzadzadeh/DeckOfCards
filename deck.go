33f8298bc9a12402d6d6ac3533a2253d5bf108dc
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-07-09 20:16:55 +0430

    create shuffle receiver function

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -39,6 +41,7 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
+// receiver function
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
@@ -51,4 +54,15 @@ func newDeckFromFile(filename string) deck {
 	}
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	// Seed for random number generator
+	source := rand.NewSource(time.Now().UnixNano())
+	// random number generator
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
