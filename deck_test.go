9e3b0e2f53faecf0a380c136d594f6447f524a9d
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-07-09 21:03:53 +0430

    Testing File IO

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -17,4 +18,16 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King Of Clubs" {
 		t.Errorf("Expected last card of King Of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
